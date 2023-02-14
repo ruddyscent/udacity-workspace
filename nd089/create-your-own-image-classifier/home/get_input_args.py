9285e6cb49884f3e66a2f68c85a9a6d807f7a6ce
Kyungwon Chun <ruddyscent@gmail.com>
2023-02-14 09:42:09 +0900

    Fix broken container

--- nd089/create-your-own-image-classifier/home/get_input_args.py
+++ nd089/create-your-own-image-classifier/home/get_input_args.py
@@ -4,11 +4,11 @@ import argparse
 
 def get_train_input_args():
     parser = argparse.ArgumentParser()
-    
+
     parser.add_argument("data_dir", type=str, help="Path to dataset folder")
     parser.add_argument("--save_dir", type=str,
                         help="Folder to store the trained model")
-    parser.add_argument("--arch", type=str, default='vgg',
+    parser.add_argument("--arch", type=str, default='vgg', choices=['vgg', 'resnet', 'alexnet'],
                         help="CNN Model (vgg, resnet, alexnet) with default value vgg")
     parser.add_argument("--learning_rate", type=float, default=1e-3,
                         help="The learning rate with default value 1e-3")
@@ -16,22 +16,22 @@ def get_train_input_args():
                         help="The number of hidden units in classifier with default value 4096")
     parser.add_argument("--epochs", type=int, default=20,
                         help="The number of epochs with default value 20")
-    parser.add_argument("--gpu", action="store_true", 
+    parser.add_argument("--gpu", action="store_true",
                         help="Enable GPU support")
-    
+
     return parser.parse_args()
 
 
 def get_predict_input_args():
     parser = argparse.ArgumentParser()
-    
+
     parser.add_argument("image_path", type=str, help="Path to input image")
     parser.add_argument("checkpoint", type=str, help="Path to model checkpoint")
     parser.add_argument("--top_k", type=int, default=3,
                         help="Number of most likely classes to return with default value 3")
     parser.add_argument("--category_names", type=str, default=None,
                         help="Path to json files which contains real categori names")
-    parser.add_argument("--gpu", action="store_true", 
+    parser.add_argument("--gpu", action="store_true",
                         help="Enable GPU support")
-    
+
     return parser.parse_args()
