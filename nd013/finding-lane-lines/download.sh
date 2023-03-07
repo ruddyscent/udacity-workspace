d998791a3de8548cf101afb0a98e783d10532982
Kyungwon Chun <ruddyscent@gmail.com>
2023-03-08 02:46:29 +0900

    Save intermediate result

--- nd013/finding-lane-lines/download.sh
+++ nd013/finding-lane-lines/download.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-wget --content-disposition 'https://www.dropbox.com/sh/hsgby5aql3s7b69/AAD1DHaE04_zQp5QTsiUufwIa\?dl\=0'
+wget --content-disposition https://www.dropbox.com/sh/hsgby5aql3s7b69/AAD1DHaE04_zQp5QTsiUufwIa\?dl\=0
 unzip test_videos.zip -x / -d home/test_videos
 wget --content-disposition https://www.dropbox.com/sh/6vtcwsa35b2ddt8/AABvTVkHogKL6s9WMxlnd3Kna?dl=0
 unzip test_images.zip -x / -d home/test_images
-rm test_videos.zip test_images.zip
+wget --content-disposition https://www.dropbox.com/sh/4ev9h034juh1ppy/AAD0BI78UVYazGP9a8xn0A34a?dl=0
+unzip examples.zip -x / -d home/examples
+rm test_videos.zip test_images.zip examples.zip
