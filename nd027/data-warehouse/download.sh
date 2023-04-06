66dd79df4e5b8702f3a8655a96d31dd267078259
Kyungwon Chun <ruddyscent@gmail.com>
2023-04-07 03:50:21 +0900

    nd027 data-warehouse (#55)
    * nd027 set container for PostgreSQL
    
    * nd027 set container for Cassandra
    
    * nd027 Set p1 starter template
    
    * Set container for p2
    
    * Commit stater templates
    
    * Resolve the ipython-sql compatibility issue
    
    * Satisfy rubric requirements
    
    * Add a required condition to insert query of songplays
    
    * Move security-sensitive files to Dropbox
    
    * Fix broken image links

---
+++ nd027/data-warehouse/download.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+rm -rf home/{dwh.cfg,L3\ Exercise\ 2\ -\ IaC\ -\ Solution.ipynb}
+
+wget -O home/dwh.cfg --content-disposition "https://www.dropbox.com/s/fxh3f90p8oom6a4/dwh.cfg?dl=0"
+
+wget -O home/L3\ Exercise\ 2\ -\ IaC\ -\ Solution.ipynb --content-disposition "https://www.dropbox.com/s/kipomudynol3574/L3%20Exercise%202%20-%20IaC%20-%20Solution.ipynb?dl=0"
