3eda0cbc143ba4fda674a20597953b55561226c3
Kyungwon Chun <ruddyscent@gmail.com>
2023-05-28 19:32:51 +0900

    Make consistence use of quotation mark

--- nd027/cloud-data-warehouses/home/create_tables.py
+++ nd027/cloud-data-warehouses/home/create_tables.py
@@ -6,7 +6,7 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
-    '''Drop tables if they exist.'''
+    """Drop tables if they exist."""
     for query in drop_table_queries:
         print(query)
         cur.execute(query)
@@ -14,7 +14,7 @@ def drop_tables(cur, conn):
 
 
 def create_tables(cur, conn):
-    '''Create tables.'''
+    """Create tables."""
     for query in create_table_queries:
         print(query)
         cur.execute(query)
@@ -23,20 +23,20 @@ def create_tables(cur, conn):
 
 def main():
     config = configparser.ConfigParser()
-    config.read('dwh.cfg')
+    config.read("dwh.cfg")
 
-    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config["CLUSTER"].values()))
     cur = conn.cursor()
 
     print(f"roleArn is {config['IAM_ROLE']['ARN']}")
 
-    redshift = boto3.client('redshift',
-                           region_name=config['AWS']['REGION'],
-                           aws_access_key_id=config['AWS']['KEY'],
-                           aws_secret_access_key=config['AWS']['SECRET']
+    redshift = boto3.client("redshift",
+                           region_name=config["AWS"]["REGION"],
+                           aws_access_key_id=config["AWS"]["KEY"],
+                           aws_secret_access_key=config["AWS"]["SECRET"]
                            )
-    myClusterProps = redshift.describe_clusters(ClusterIdentifier=config['DWH']['DWH_CLUSTER_IDENTIFIER'])['Clusters'][0]
-    if myClusterProps['ClusterStatus'] == 'available':
+    myClusterProps = redshift.describe_clusters(ClusterIdentifier=config["DWH"]["DWH_CLUSTER_IDENTIFIER"])["Clusters"][0]
+    if myClusterProps["ClusterStatus"] == "available":
         print("Cluster is available")
 
     print(f"Endpoint is {myClusterProps['Endpoint']['Address']}")
