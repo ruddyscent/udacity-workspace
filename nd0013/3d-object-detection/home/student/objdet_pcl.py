ed16a344a11c92d5f19a80e2075424ed7d40b7c7
Kyungwon Chun <ruddyscent@gmail.com>
2023-07-19 16:59:12 +0900

    Complete step 2

--- nd0013/3d-object-detection/home/student/objdet_pcl.py
+++ nd0013/3d-object-detection/home/student/objdet_pcl.py
@@ -10,14 +10,17 @@
 # ----------------------------------------------------------------------
 #
 
-# general package imports
-import cv2
-import numpy as np
-import torch
-
-# add project directory to python path to enable relative imports
+# general pcackage imports
 import os
 import sys
+import cv2
+import open3d as o3d
+import numpy as np
+import torch
+import zlib
+from matplotlib import pyplot as plt
+
+# add project directory to python path to enable relative imports
 PACKAGE_PARENT = '..'
 SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
 sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
@@ -38,14 +41,26 @@ def show_pcl(pcl):
     print("student task ID_S1_EX2")
 
     # step 1 : initialize open3d with key callback and create window
+    def closeWindow(vis):
+        vis.close()
+        return False
+
+    vis = o3d.visualization.VisualizerWithKeyCallback()
+    vis.create_window(window_name='Open3D', width=1280, height=720, left=50, top=50, visible=True)
+    vis.register_key_callback(262, closeWindow)
     
     # step 2 : create instance of open3d point-cloud class
+    pcd = o3d.geometry.PointCloud()
 
     # step 3 : set points in pcd instance by converting the point-cloud into 3d vectors (using open3d function Vector3dVector)
+    pcd.points = o3d.utility.Vector3dVector(pcl[:,:3])
 
     # step 4 : for the first frame, add the pcd instance to visualization using add_geometry; for all other frames, use update_geometry instead
-    
+    vis.add_geometry(pcd)
+
     # step 5 : visualize point cloud and keep window open until right-arrow is pressed (key-code 262)
+    # vis.update_geometry(pcd)
+    vis.run()
 
     #######
     ####### ID_S1_EX2 END #######     
@@ -59,18 +74,35 @@ def show_range_image(frame, lidar_name):
     print("student task ID_S1_EX1")
 
     # step 1 : extract lidar data and range image for the roof-mounted lidar
-    
+    lidar = [obj for obj in frame.lasers if obj.name == lidar_name][0] # get laser data structure from frame
+  
     # step 2 : extract the range and the intensity channel from the range image
-    
+    if len(lidar.ri_return1.range_image_compressed) > 0: # use first response
+        ri = dataset_pb2.MatrixFloat()
+        ri.ParseFromString(zlib.decompress(lidar.ri_return1.range_image_compressed))
+        ri = np.array(ri.data).reshape(ri.shape.dims)
+      
     # step 3 : set values <0 to zero
-    
+    ri[ri<0] = 0.0
+
     # step 4 : map the range channel onto an 8-bit scale and make sure that the full range of values is appropriately considered
-    
+    ri_range = ri[:,:,0]
+    ri_range = ri_range * 255 / (np.amax(ri_range) - np.amin(ri_range))
+    img_range = ri_range.astype(np.uint8)
+
     # step 5 : map the intensity channel onto an 8-bit scale and normalize with the difference between the 1- and 99-percentile to mitigate the influence of outliers
-    
+    ri_intensity = ri[:,:,1]
+    ri_intensity = np.amax(ri_intensity)/2 * ri_intensity * 255 / (np.amax(ri_intensity) - np.amin(ri_intensity)) 
+    img_intensity = ri_intensity.astype(np.uint8)
+
     # step 6 : stack the range and intensity image vertically using np.vstack and convert the result to an unsigned 8-bit integer
+    img_range_intensity = np.vstack((img_range, img_intensity)).astype(np.uint8)
     
-    img_range_intensity = [] # remove after implementing all steps
+    # step 7: focus on +/- 90 deg. left and right of the forward-facing x-axis
+    deg90 = int(img_range_intensity.shape[1] / 4)
+    img_range_intensity_center = int(img_range_intensity.shape[1] / 2)
+    img_range_intensity = img_range_intensity[:, img_range_intensity_center - deg90:img_range_intensity_center + deg90]
+ 
     #######
     ####### ID_S1_EX1 END #######     
     
@@ -95,14 +127,18 @@ def bev_from_pcl(lidar_pcl, configs):
     print("student task ID_S2_EX1")
 
     ## step 1 :  compute bev-map discretization by dividing x-range by the bev-image height (see configs)
+    bev_discret = (configs.lim_x[1] - configs.lim_x[0]) / configs.bev_height
 
     ## step 2 : create a copy of the lidar pcl and transform all metrix x-coordinates into bev-image coordinates    
+    lidar_pcl_cpy = np.copy(lidar_pcl)
+    lidar_pcl_cpy[:, 0] = np.int_(np.floor(lidar_pcl_cpy[:, 0] / bev_discret))
 
     # step 3 : perform the same operation as in step 2 for the y-coordinates but make sure that no negative bev-coordinates occur
+    lidar_pcl_cpy[:, 1] = np.int_(np.floor(lidar_pcl_cpy[:, 1] / bev_discret) + (configs.bev_width + 1) / 2)
 
     # step 4 : visualize point-cloud using the function show_pcl from a previous task
-    
-    #######
+    # show_pcl(lidar_pcl_cpy)
+
     ####### ID_S2_EX1 END #######     
     
     
@@ -112,17 +148,26 @@ def bev_from_pcl(lidar_pcl, configs):
     print("student task ID_S2_EX2")
 
     ## step 1 : create a numpy array filled with zeros which has the same dimensions as the BEV map
+    intensity_map = np.zeros((configs.bev_height + 1, configs.bev_width + 1))
 
     # step 2 : re-arrange elements in lidar_pcl_cpy by sorting first by x, then y, then -z (use numpy.lexsort)
+    idx_intensity = np.lexsort((-lidar_pcl_cpy[:, 2], lidar_pcl_cpy[:, 1], lidar_pcl_cpy[:, 0]))
+    lidar_top_pcl = lidar_pcl_cpy[idx_intensity]
 
     ## step 3 : extract all points with identical x and y such that only the top-most z-coordinate is kept (use numpy.unique)
     ##          also, store the number of points per x,y-cell in a variable named "counts" for use in the next task
+    _, idx_intensity_unique = np.unique(lidar_top_pcl[:, 0:2], axis=0, return_index=True)
+    lidar_top_pcl = lidar_top_pcl[idx_intensity_unique]
 
     ## step 4 : assign the intensity value of each unique entry in lidar_top_pcl to the intensity map 
     ##          make sure that the intensity is scaled in such a way that objects of interest (e.g. vehicles) are clearly visible    
     ##          also, make sure that the influence of outliers is mitigated by normalizing intensity on the difference between the max. and min. value within the point cloud
-
+    intensity_map[np.int_(lidar_top_pcl[:, 0]), np.int_(lidar_top_pcl[:, 1])] = lidar_top_pcl[:, 2] / float(np.abs(configs.lim_z[1] - configs.lim_z[0]))
+    
     ## step 5 : temporarily visualize the intensity map using OpenCV to make sure that vehicles separate well from the background
+    # intensity_map = np.clip(intensity_map * 255, 0, 255).astype(np.uint8)
+    # plt.imshow(intensity_map)
+    # plt.pause(0)
 
     #######
     ####### ID_S2_EX2 END ####### 
@@ -134,27 +179,32 @@ def bev_from_pcl(lidar_pcl, configs):
     print("student task ID_S2_EX3")
 
     ## step 1 : create a numpy array filled with zeros which has the same dimensions as the BEV map
+    height_map = np.zeros((configs.bev_height + 1, configs.bev_width + 1))
 
     ## step 2 : assign the height value of each unique entry in lidar_top_pcl to the height map 
     ##          make sure that each entry is normalized on the difference between the upper and lower height defined in the config file
     ##          use the lidar_pcl_top data structure from the previous task to access the pixels of the height_map
+    height_map[np.int_(lidar_top_pcl[:, 0]), np.int_(lidar_top_pcl[:, 1])] = lidar_top_pcl[:, 2] / float(np.abs(configs.lim_z[1] - configs.lim_z[0]))
 
     ## step 3 : temporarily visualize the intensity map using OpenCV to make sure that vehicles separate well from the background
+    # height_map = np.clip(height_map * 255, 0, 255).astype(np.uint8)
+    # plt.imshow(height_map)
+    # plt.pause(0)
 
     #######
     ####### ID_S2_EX3 END #######       
 
-    # TODO remove after implementing all of the above steps
-    lidar_pcl_cpy = []
-    lidar_pcl_top = []
-    height_map = []
-    intensity_map = []
+    # # TODO remove after implementing all of the above steps
+    # lidar_pcl_cpy = []
+    # lidar_pcl_top = []
+    # height_map = []
+    # intensity_map = []
 
     # Compute density layer of the BEV map
     density_map = np.zeros((configs.bev_height + 1, configs.bev_width + 1))
     _, _, counts = np.unique(lidar_pcl_cpy[:, 0:2], axis=0, return_index=True, return_counts=True)
     normalizedCounts = np.minimum(1.0, np.log(counts + 1) / np.log(64)) 
-    density_map[np.int_(lidar_pcl_top[:, 0]), np.int_(lidar_pcl_top[:, 1])] = normalizedCounts
+    density_map[np.int_(lidar_top_pcl[:, 0]), np.int_(lidar_top_pcl[:, 1])] = normalizedCounts
         
     # assemble 3-channel bev-map from individual maps
     bev_map = np.zeros((3, configs.bev_height, configs.bev_width))
--- nd0013/3d-object-detection/home/writeup.md
+++ nd0013/3d-object-detection/home/writeup.md
@@ -1,6 +1,19 @@
-# Writeup: Track 3D-Objects Over Time
+# Writeup: 3D Objects Detection
 
-Please use this starter template to answer the following questions:
+## Visualize point-cloud
+### Find 10 examples of vehicles with varying degrees of visibility in the point-cloud
+![Open3D Point Cloud Screenshot 1](./img/o3d_pcd_1.png)
+![Open3D Point Cloud Screenshot 2](./img/o3d_pcd_2.png)
+![Open3D Point Cloud Screenshot 3](./img/o3d_pcd_3.png)
+![Open3D Point Cloud Screenshot 4](./img/o3d_pcd_4.png)
+![Open3D Point Cloud Screenshot 5](./img/o3d_pcd_5.png)
+![Open3D Point Cloud Screenshot 6](./img/o3d_pcd_6.png)
+![Open3D Point Cloud Screenshot 7](./img/o3d_pcd_7.png)
+![Open3D Point Cloud Screenshot 8](./img/o3d_pcd_8.png)
+![Open3D Point Cloud Screenshot 9](./img/o3d_pcd_9.png)
+
+### Try to identify vehicle features that appear stable in most of the inspected examples and describe them.
+Depending on the position and orientation of the vehicle, the shading only allows you to see certain parts of the car, so you can't pick up consistent features. However, we can see that the side-view mirrors work well for certain vehicle orientations. You can also see that the windows of the car, which are silhouetted by the bonnet and the respective pillars, are also well captured depending on the orientation. The wheels are also a characteristic feature well-defined when photographing the vehicle from the side.
 
 ### 1. Write a short recap of the four tracking steps and what you implemented there (filter, track management, association, camera fusion). Which results did you achieve? Which part of the project was most difficult for you to complete, and why?
 
