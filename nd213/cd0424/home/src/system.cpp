0e6124701156f6cdcca26f08d5a633668643da24
Kyungwon Chun <ruddyscent@gmail.com>
2023-09-11 01:50:53 +0900

    Display operating system

--- nd213/cd0424/home/src/system.cpp
+++ nd213/cd0424/home/src/system.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 
+#include "linux_parser.h"
 #include "process.h"
 #include "processor.h"
 #include "system.h"
@@ -28,8 +29,8 @@ std::string System::Kernel() { return string(); }
 // TODO: Return the system's memory utilization
 float System::MemoryUtilization() { return 0.0; }
 
-// TODO: Return the operating system name
-std::string System::OperatingSystem() { return string(); }
+// DONE: Return the operating system name
+std::string System::OperatingSystem() { return LinuxParser::OperatingSystem(); }
 
 // TODO: Return the number of processes actively running on the system
 int System::RunningProcesses() { return 0; }
