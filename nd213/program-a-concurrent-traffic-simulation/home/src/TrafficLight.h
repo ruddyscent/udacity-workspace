12deae575c9923b374a0ac8b64bd003345596f3e
Kyungwon Chun <ruddyscent@gmail.com>
2023-10-17 18:12:24 +0900

    Complete FP.3

--- nd213/program-a-concurrent-traffic-simulation/home/src/TrafficLight.h
+++ nd213/program-a-concurrent-traffic-simulation/home/src/TrafficLight.h
@@ -19,9 +19,13 @@ template <class T>
 class MessageQueue
 {
 public:
+    T receive();
+    void send(T &&msg);
 
 private:
-    
+    std::mutex _mutex;
+    std::condition_variable _condition;
+    std::deque<T> _queue;
 };
 
 // FP.1 : Define a class „TrafficLight“ which is a child class of TrafficObject. 
