d428c9463958805588cec1a5a8883eb9a7c6f74f
Kyungwon Chun <ruddyscent@gmail.com>
2023-10-19 00:00:28 +0900

    Read configuration from file

--- nd213/capstone/home/src/main.cpp
+++ nd213/capstone/home/src/main.cpp
@@ -1,22 +1,82 @@
 #include <iostream>
+#include <sstream>
+#include <fstream>
+#include <vector>
+
 #include "controller.h"
 #include "game.h"
 #include "renderer.h"
 
-int main() {
-  constexpr std::size_t kFramesPerSecond{60};
-  constexpr std::size_t kMsPerFrame{1000 / kFramesPerSecond};
-  constexpr std::size_t kScreenWidth{640};
-  constexpr std::size_t kScreenHeight{640};
-  constexpr std::size_t kGridWidth{32};
-  constexpr std::size_t kGridHeight{32};
+using namespace std;
+
+vector<string> split(const string &str, char delim = ' ')
+{
+  vector<string> tokens;
+  string token;
+  istringstream tokenStream(str);
+  while (getline(tokenStream, token, delim))
+  {
+    if (token.empty())
+      continue;
+    tokens.push_back(token);
+  }
+  return tokens;
+}
+
+int main(int argc, char *argv[])
+{
+  if (argc != 2)
+  {
+    cerr << "Usage: " << argv[0] << " CONFIG_FILE\n";
+    return EXIT_FAILURE;
+  }
+
+  ifstream input_file(argv[1], ios::in);
+  if (!input_file.is_open())
+  {
+    cerr << "Could not open input file '" << argv[1] << "'!\n";
+    return EXIT_FAILURE;
+  }
+
+  size_t kFramesPerSecond{60};
+  size_t kMsPerFrame{1000 / kFramesPerSecond};
+  size_t kScreenWidth{640};
+  size_t kScreenHeight{640};
+  size_t kGridWidth{32};
+  size_t kGridHeight{32};
+
+  string buffer;
+  while (getline(input_file, buffer))
+  {
+    vector<string> splitted = split(buffer, ' ');
+    if (splitted.size() != 2)
+    {
+      cerr << "Invalid input file format: " << buffer << "\n";
+      return EXIT_FAILURE;
+    }
+    else if (splitted[0] == "FramesPerSecond")
+      kFramesPerSecond = stoi(splitted[1]);
+    else if (splitted[0] == "ScreenWidth")
+      kScreenWidth = stoi(splitted[1]);
+    else if (splitted[0] == "ScreenHeight")
+      kScreenHeight = stoi(splitted[1]);
+    else if (splitted[0] == "GridWidth")
+      kGridWidth = stoi(splitted[1]);
+    else if (splitted[0] == "GridHeight")
+      kGridHeight = stoi(splitted[1]);
+    else
+    {
+      cerr << "Invalid keyword: " << buffer << "\n";
+      return EXIT_FAILURE;
+    }
+  }
 
   Renderer renderer(kScreenWidth, kScreenHeight, kGridWidth, kGridHeight);
   Controller controller;
   Game game(kGridWidth, kGridHeight);
   game.Run(controller, renderer, kMsPerFrame);
-  std::cout << "Game has terminated successfully!\n";
-  std::cout << "Score: " << game.GetScore() << "\n";
-  std::cout << "Size: " << game.GetSize() << "\n";
+  cout << "Game has terminated successfully!\n";
+  cout << "Score: " << game.GetScore() << "\n";
+  cout << "Size: " << game.GetSize() << "\n";
   return 0;
 }
