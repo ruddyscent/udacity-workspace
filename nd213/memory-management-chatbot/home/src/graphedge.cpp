005d731935dff7a26e925366532caaf5bfea8e88
Kyungwon Chun <ruddyscent@gmail.com>
2024-01-29 03:31:54 +0900

    Use constant reference

--- nd213/memory-management-chatbot/home/src/graphedge.cpp
+++ nd213/memory-management-chatbot/home/src/graphedge.cpp
@@ -13,7 +13,7 @@ void GraphEdge::SetParentNode(GraphNode *parentNode)
     _parentNode = parentNode;
 }
 
-void GraphEdge::AddToken(std::string token)
+void GraphEdge::AddToken(const std::string& token)
 {
     _keywords.push_back(token);
 }
