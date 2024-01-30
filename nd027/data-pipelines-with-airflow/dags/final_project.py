10e21bfabdfb0912a046076208fb5c8d4c996ab0
Kyungwon Chun <ruddyscent@gmail.com>
2024-01-30 14:34:30 +0900

    Use usual argument passing.

--- nd027/data-pipelines-with-airflow/dags/final_project.py
+++ nd027/data-pipelines-with-airflow/dags/final_project.py
@@ -28,87 +28,71 @@ def final_project():
 
     stage_events_to_redshift = StageToRedshiftOperator(
         task_id="Stage_events",
-        params={
-            "table": "staging_events",
-            "redshift_conn_id": "redshift",
-            "aws_credentials_id": "aws_credentials",
-            "s3_bucket": "udacity-dend",
-            "s3_key": "log-data/",
-            "region": "us-west-2",
-            "json_path": "s3://udacity-dend/log_json_path.json"
-         }
+        table="staging_events",
+        redshift_conn_id="redshift",
+        aws_credentials_id="aws_credentials",
+        s3_bucket="udacity-dend",
+        s3_key="log-data/",
+        region="us-west-2",
+        json_path="s3://udacity-dend/log_json_path.json"
     )
 
     stage_songs_to_redshift = StageToRedshiftOperator(
         task_id="Stage_songs",
-        params={
-            "table": "staging_songs",
-            "redshift_conn_id": "redshift",
-            "aws_credentials_id": "aws_credentials",
-            "s3_bucket": "udacity-dend",
-            "s3_key": "song-data/",
-            "region": "us-west-2",
-        }
+        table="staging_songs",
+        redshift_conn_id="redshift",
+        aws_credentials_id="aws_credentials",
+        s3_bucket="udacity-dend",
+        s3_key="song-data/A/A",
+        region="us-west-2"
     )
 
     load_songplays_table = LoadFactOperator(
         task_id="Load_songplays_fact_table",
-        params={
-            "redshift_conn_id": "redshift",
-            "table": "songplays",
-            "sql_statement": SqlQueries.songplay_table_insert
-        }
+        redshift_conn_id="redshift",
+        table="songplays",
+        sql_statement=SqlQueries.songplay_table_insert
     )
 
     load_user_dimension_table = LoadDimensionOperator(
         task_id="Load_user_dim_table",
-        params={
-            "redshift_conn_id": "redshift",
-            "table": "users",
-            "sql_statement": SqlQueries.user_table_insert
-        }
+        redshift_conn_id="redshift",
+        table="users",
+        sql_statement=SqlQueries.user_table_insert
     )
 
     load_song_dimension_table = LoadDimensionOperator(
         task_id="Load_song_dim_table",
-        params={
-            "redshift_conn_id": "redshift",
-            "table": "songs",
-            "sql_statement": SqlQueries.song_table_insert
-        }
+        redshift_conn_id="redshift",
+        table="songs",
+        sql_statement=SqlQueries.song_table_insert
     )
 
     load_artist_dimension_table = LoadDimensionOperator(
         task_id="Load_artist_dim_table",
-        params={
-            "redshift_conn_id": "redshift",
-            "table": "artists",
-            "sql_statement": SqlQueries.artist_table_insert
-        }
+        redshift_conn_id="redshift",
+        table="artists",
+        sql_statement=SqlQueries.artist_table_insert
     )
 
     load_time_dimension_table = LoadDimensionOperator(
         task_id="Load_time_dim_table",
-        params={
-            "redshift_conn_id": "redshift",
-            "table": "time",
-            "sql_statement": SqlQueries.time_table_insert
-        }
+        redshift_conn_id="redshift",
+        table="time",
+        sql_statement=SqlQueries.time_table_insert
     )
 
     run_quality_checks = DataQualityOperator(
         task_id="Run_data_quality_checks",
-        params={
-            "redshift_conn_id": "redshift",
-            "tests": [{"sql": "SELECT COUNT(*) FROM time WHERE start_time IS NULL",
-                     "expected_result": 0},
-                     {"sql": "SELECT COUNT(*) FROM users WHERE userid IS NULL",
-                     "expected_result": 0},
-                     {"sql": "SELECT COUNT(*) FROM songs WHERE songid IS NULL",
-                     "expected_result": 0},
-                     {"sql": "SELECT COUNT(*) FROM artists WHERE artistid IS NULL",
-                     "expected_result": 0}],
-            }
+        redshift_conn_id="redshift",
+        tests=[{"sql": "SELECT COUNT(*) FROM time WHERE start_time IS NULL",
+                "expected_result": 0},
+                {"sql": "SELECT COUNT(*) FROM users WHERE userid IS NULL",
+                "expected_result": 0},
+                {"sql": "SELECT COUNT(*) FROM songs WHERE songid IS NULL",
+                "expected_result": 0},
+                {"sql": "SELECT COUNT(*) FROM artists WHERE artistid IS NULL",
+                "expected_result": 0}]
     )
 
     end_operator = DummyOperator(task_id="Stop_execution")
