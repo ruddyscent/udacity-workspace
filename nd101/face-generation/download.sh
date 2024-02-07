e297d8e32679ab615f020336896f59d978b9af6e
Kyungwon Chun <ruddyscent@gmail.com>
2024-02-08 02:01:38 +0900

    nd104 cd1823 Face Generation (#126)
    * Use lesson code for container.
    
    * Update sample image.
    
    * Upload dataset to Dropbox.

---
+++ nd101/face-generation/download.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+rm -rf home/processed-celeba-small.zip
+
+wget -O home/processed-celeba-small.zip --content-disposition "https://www.dropbox.com/scl/fi/cvrss584pr7hqa1dpbx0p/processed-celeba-small.zip?rlkey=g0gouei2ep7jizp00nlx3r8yf&dl=0"
